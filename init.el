e4cf2f9cf0ec513c8e52f403eef85704026f5236
Michael Utz <michael@theutz.com>
2020-03-14 21:12:33 +0200

    Bootstrap literate config

--- init.el
+++ init.el
@@ -2,26 +2,43 @@
 ;; ==============================
 
 ;;; Commentary:
-;;
+;; ------------
 ;; "We don't have mistakes. We just have happy accidents."
 
 ;;; Code:
+;; ------
 
-;;; straight.el
-;; ------------
-;; "There's nothing wrong with having a tree as a friend."
+(let ((gc-cons-threshold most-positive-fixnum))
+  
+  ;;; straight.el
+  ;; ------------
+  ;; "There's nothing wrong with having a tree as a friend."
 
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+  (defvar bootstrap-version)
+  (let ((bootstrap-file
+	 (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+	(bootstrap-version 5))
+    (unless (file-exists-p bootstrap-file)
+      (with-current-buffer
+	  (url-retrieve-synchronously
+	   "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+	   'silent 'inhibit-cookies)
+	(goto-char (point-max))
+	(eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage))
 
+  ;;; org-mode
+  ;; ---------
+  ;; “No pressure. Just relax and watch it happen.”
+  
+  (straight-use-package 'org)
+  (require 'org)
+
+  (org-babel-load-file
+   (expand-file-name "the-canvas.org" user-emacs-directory))
+
+  ;; “See how it fades right into nothing. That’s just what you’re looking for.”
+  (garbage-collect))
+
+(provide 'init)
 ;;; init.el ends here
